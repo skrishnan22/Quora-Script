adf8be18c2fb6c97bee994b46397429bf2db7703
skrishnan22 <skrish22195@gmail.com>
2015-12-11 23:33:35 +0530

    changed innerHTML to textContent
    Change done so that the script works in firefox also

--- Quora.js
+++ Quora.js
@@ -6,7 +6,7 @@ function remove()
    
    for(var i=0;i<answers.length;i++)
    {
-      var upvotes=document.getElementsByClassName('Answer Upvote Button TwoStateButton primary_action answer_upvote main_button')[i].innerText;
+      var upvotes=document.getElementsByClassName('Answer Upvote Button TwoStateButton primary_action answer_upvote main_button')[i].textContent;
       upvotes=upvotes.replace('Upvote','' ).replace('k','000').replace('.','');
       var upvote_no=parseInt(upvotes);
       if(upvote_no<limit)    
