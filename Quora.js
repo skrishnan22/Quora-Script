1da9e409dd037f72a27636593533a096fe18fe9d
skrishnan22 <skrish22195@gmail.com>
2015-12-11 23:19:30 +0530

    Indented the code

--- Quora.js
+++ Quora.js
@@ -1,21 +1,21 @@
 
-function remove(){
-var answers= document.getElementsByClassName('Answer Upvote Button TwoStateButton primary_action answer_upvote main_button');
-var limit=500;
-
-for(var i=0;i<answers.length;i++)
+function remove()
 {
-var upvotes=document.getElementsByClassName('Answer Upvote Button TwoStateButton primary_action answer_upvote main_button')[i].innerText;
-upvotes=upvotes.replace('Upvote','' ).replace('k','000').replace('.','');
-var upvote_no=parseInt(upvotes);
-if(upvote_no<limit)    
-{
-var feed_id=document.getElementsByClassName('feed_item feed_type_answer')[i].id;
-document.getElementById(feed_id).remove();    
-   } 
-}
+   var answers= document.getElementsByClassName('Answer Upvote Button TwoStateButton primary_action answer_upvote main_button');
+   var limit=500;
+   
+   for(var i=0;i<answers.length;i++)
+   {
+      var upvotes=document.getElementsByClassName('Answer Upvote Button TwoStateButton primary_action answer_upvote main_button')[i].innerText;
+      upvotes=upvotes.replace('Upvote','' ).replace('k','000').replace('.','');
+      var upvote_no=parseInt(upvotes);
+      if(upvote_no<limit)    
+      {
+         var feed_id=document.getElementsByClassName('feed_item feed_type_answer')[i].id;
+         document.getElementById(feed_id).remove();    
+      } 
+   }
 }
 document.addEventListener("DOMNodeInserted", function() {
-            
-            remove();
+   remove();
         });
